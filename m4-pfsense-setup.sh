375bbdf3f59aa91319d81b02b543c3dca5b3d37d
m4services <murilo.melo@m4services.com.br>
2024-09-30 16:44:06 -0300

    Update m4-pfsense-setup.sh

--- m4-pfsense-setup.sh
+++ m4-pfsense-setup.sh
@@ -11,28 +11,11 @@ echo "Instalando o Zabbix Agent..."
 pkg update
 pkg install -y zabbix-agent6  # ou zabbix-agent5, dependendo do que você preferir
 
-# 3. Adicionar User Parameters ao Zabbix Agent
-echo "Adicionando User Parameters ao Zabbix Agent..."
-cat << EOF >> /usr/local/etc/zabbix6/zabbix_agentd.conf
-AllowRoot=1
-UserParameter=pfsense.states.max,grep "limit states" /tmp/rules.limits | cut -f4 -d ' '
-UserParameter=pfsense.states.current,grep "current entries" /tmp/pfctl_si_out | tr -s ' ' | cut -f4 -d ' '
-UserParameter=pfsense.mbuf.current,netstat -m | grep "mbuf clusters" | cut -f1 -d ' ' | cut -d '/' -f1
-UserParameter=pfsense.mbuf.cache,netstat -m | grep "mbuf clusters" | cut -f1 -d ' ' | cut -d '/' -f2
-UserParameter=pfsense.mbuf.max,netstat -m | grep "mbuf clusters" | cut -f1 -d ' ' | cut -d '/' -f4
-UserParameter=pfsense.discovery[*],/usr/local/bin/php /root/scripts/pfsense_zbx.php discovery \$1
-UserParameter=pfsense.value[*],/usr/local/bin/php /root/scripts/pfsense_zbx.php \$1 \$2 \$3
-EOF
-
-# 4. Aumentar o Timeout
-echo "Aumentando o Timeout do Zabbix Agent..."
-sed -i '' 's/^Timeout=.*/Timeout=5/' /usr/local/etc/zabbix6/zabbix_agentd.conf
-
-# 5. Configurar cronjob para a versão do sistema
+# 3. Configurar cronjob para a versão do sistema
 echo "Configurando cronjob para a versão do sistema..."
 /usr/local/bin/php /root/scripts/pfsense_zbx.php sysversion_cron
 
-# 6. Verificar e instalar o pacote speedtest
+# 4. Verificar e instalar o pacote speedtest
 echo "Verificando o pacote speedtest..."
 PACKAGE_NAME=$(pkg search speedtest | grep -oE 'py[0-9]+-speedtest-cli')
 
@@ -44,17 +27,21 @@ fi
 echo "Instalando o pacote $PACKAGE_NAME..."
 pkg install -y $PACKAGE_NAME
 
-# 7. Testar se o speedtest foi instalado corretamente
+# 5. Testar se o speedtest foi instalado corretamente
 echo "Testando a instalação do speedtest..."
 /usr/local/bin/speedtest
 
 if [ $? -ne 0 ]; then
     echo "Erro ao executar speedtest. Baixando o script mais recente..."
-    curl -Lo /usr/local/lib/python3.8/site-packages/speedtest.py https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
+    curl -Lo /usr/local/lib/python3.11/site-packages/speedtest.py https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
 fi
 
-# 8. Configurar cronjob para o Speedtest
+# 6. Configurar cronjob para o Speedtest
 echo "Configurando cronjob para o Speedtest..."
 /usr/local/bin/php /root/scripts/pfsense_zbx.php speedtest_cron
 
 echo "Configuração do pfSense concluída."
+
+echo "EDITE O TIMEOUT."
+
+echo "EDITE O USERPARAMETERS DO ZABBIX AGENT."
