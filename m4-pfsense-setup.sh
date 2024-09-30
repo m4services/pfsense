61f735ba1142745565d019426871f2b4eea98da5
m4services <murilo.melo@m4services.com.br>
2024-09-30 16:01:49 -0300

    Update m4-pfsense-setup.sh

--- m4-pfsense-setup.sh
+++ m4-pfsense-setup.sh
@@ -4,9 +4,16 @@
 echo "Baixando o arquivo pfsense_zbx.php..."
 curl --create-dirs -o /root/scripts/pfsense_zbx.php https://raw.githubusercontent.com/rbicelli/pfsense-zabbix-template/master/pfsense_zbx.php
 
+# Verifica se o arquivo de configuração do Zabbix existe
+CONFIG_FILE="/usr/local/etc/zabbix6/zabbix_agentd.conf"
+if [ ! -f "$CONFIG_FILE" ]; then
+    echo "Arquivo de configuração do Zabbix não encontrado: $CONFIG_FILE"
+    exit 1
+fi
+
 # Remove parâmetros de usuário existentes do Zabbix Agent
 echo "Removendo parâmetros de usuário existentes..."
-sed -i '' '/^UserParameter=/d' /usr/local/etc/zabbix6/zabbix_agentd.conf
+sed -i '' '/^UserParameter=/d' "$CONFIG_FILE"
 
 # Configura os novos parâmetros de usuário para o Zabbix Agent
 echo "Configurando Zabbix Agent..."
@@ -19,11 +26,11 @@ echo "Configurando Zabbix Agent..."
     echo "UserParameter=pfsense.mbuf.max,netstat -m | grep \"mbuf clusters\" | cut -f1 -d ' ' | cut -d '/' -f4"
     echo "UserParameter=pfsense.discovery[*],/usr/local/bin/php /root/scripts/pfsense_zbx.php discovery \$1"
     echo "UserParameter=pfsense.value[*],/usr/local/bin/php /root/scripts/pfsense_zbx.php \$1 \$2 \$3"
-} >> /usr/local/etc/zabbix6/zabbix_agentd.conf
+} >> "$CONFIG_FILE"
 
-# Aumenta o valor do timeout para 30
-echo "Aumentando o Timeout para 30..."
-sed -i '' 's/^# Timeout=10/Timeout=30/' /usr/local/etc/zabbix6/zabbix_agentd.conf
+# Aumenta o valor do timeout para 5
+echo "Aumentando o Timeout para 5..."
+sed -i '' 's/^# Timeout=3/Timeout=5/' "$CONFIG_FILE"
 
 # Reinicia o serviço do Zabbix Agent para aplicar as mudanças
 echo "Reiniciando o Zabbix Agent..."
