e6d49f784ca40559281813792ce4a4dcc4593c21
m4services <murilo.melo@m4services.com.br>
2024-09-30 16:32:31 -0300

    Update m4-pfsense-setup.sh

--- m4-pfsense-setup.sh
+++ m4-pfsense-setup.sh
@@ -4,31 +4,34 @@
 echo "Baixando o arquivo pfsense_zbx.php..."
 curl --create-dirs -o /root/scripts/pfsense_zbx.php https://raw.githubusercontent.com/rbicelli/pfsense-zabbix-template/master/pfsense_zbx.php
 
-# Remove parâmetros de usuário existentes
-echo "Removendo parâmetros de usuário existentes..."
-sed -i '' '/^UserParameter=/d' /usr/local/etc/zabbix6/zabbix_agentd.conf
+# Define o arquivo de configuração
+CONF_FILE="/usr/local/etc/zabbix6/zabbix_agentd.conf"
 
-# Edita o Timeout para 30
-echo "Configurando o Timeout para 30..."
-sed -i '' '/^Timeout=/d' /usr/local/etc/zabbix6/zabbix_agentd.conf
-sed -i '' '/^BufferSend=/i\
-Timeout=30\
-' /usr/local/etc/zabbix6/zabbix_agentd.conf
+# Cria um backup do arquivo de configuração
+cp $CONF_FILE ${CONF_FILE}.bak
 
-# Adiciona AllowRoot e UserParameters na posição correta
-echo "Configurando Zabbix Agent..."
+# Edita o arquivo de configuração
+awk -v ORS= -v timeout=30 '
 {
-  echo
-  echo "AllowRoot=1"
-  echo
-  echo "UserParameter=pfsense.states.max,grep \"limit states\" /tmp/rules.limits | cut -f4 -d ' '"
-  echo "UserParameter=pfsense.states.current,grep \"current entries\" /tmp/pfctl_si_out | tr -s ' ' | cut -f4 -d ' '"
-  echo "UserParameter=pfsense.mbuf.current,netstat -m | grep \"mbuf clusters\" | cut -f1 -d ' ' | cut -d '/' -f1"
-  echo "UserParameter=pfsense.mbuf.cache,netstat -m | grep \"mbuf clusters\" | cut -f1 -d ' ' | cut -d '/' -f2"
-  echo "UserParameter=pfsense.mbuf.max,netstat -m | grep \"mbuf clusters\" | cut -f1 -d ' ' | cut -d '/' -f4"
-  echo "UserParameter=pfsense.discovery[*],/usr/local/bin/php /root/scripts/pfsense_zbx.php discovery \$1"
-  echo "UserParameter=pfsense.value[*],/usr/local/bin/php /root/scripts/pfsense_zbx.php \$1 \$2 \$3"
-} >> /usr/local/etc/zabbix6/zabbix_agentd.conf
+    if ($0 ~ /^Timeout=/) {
+        print "Timeout=" timeout "\n"
+        next
+    }
+    print $0 "\n"
+}
+END {
+    print "\nAllowRoot=1\n"
+    print "UserParameter=pfsense.states.max,grep \"limit states\" /tmp/rules.limits | cut -f4 -d ' '\n"
+    print "UserParameter=pfsense.states.current,grep \"current entries\" /tmp/pfctl_si_out | tr -s ' ' | cut -f4 -d ' '\n"
+    print "UserParameter=pfsense.mbuf.current,netstat -m | grep \"mbuf clusters\" | cut -f1 -d ' ' | cut -d '/' -f1\n"
+    print "UserParameter=pfsense.mbuf.cache,netstat -m | grep \"mbuf clusters\" | cut -f1 -d ' ' | cut -d '/' -f2\n"
+    print "UserParameter=pfsense.mbuf.max,netstat -m | grep \"mbuf clusters\" | cut -f1 -d ' ' | cut -d '/' -f4\n"
+    print "UserParameter=pfsense.discovery[*],/usr/local/bin/php /root/scripts/pfsense_zbx.php discovery $1\n"
+    print "UserParameter=pfsense.value[*],/usr/local/bin/php /root/scripts/pfsense_zbx.php $1 $2 $3\n"
+}' $CONF_FILE > /tmp/zabbix_agentd.conf
+
+# Sobrescreve o arquivo de configuração original
+mv /tmp/zabbix_agentd.conf $CONF_FILE
 
 # Reinicia o serviço do Zabbix Agent para aplicar as mudanças
 echo "Reiniciando o Zabbix Agent..."
