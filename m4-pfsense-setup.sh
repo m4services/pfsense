cd3d09c98d860a208475036af6e53628c0587f05
m4services <murilo.melo@m4services.com.br>
2024-09-30 15:58:21 -0300

    Update m4-pfsense-setup.sh

--- m4-pfsense-setup.sh
+++ m4-pfsense-setup.sh
@@ -1,21 +1,16 @@
 #!/bin/sh
 
-# Definindo o caminho do arquivo de configuração do Zabbix Agent
-ZABBIX_CONF="/usr/local/etc/zabbix6/zabbix_agentd.conf"
-
 # Baixa o arquivo pfsense_zbx.php para o diretório /root/scripts
 echo "Baixando o arquivo pfsense_zbx.php..."
 curl --create-dirs -o /root/scripts/pfsense_zbx.php https://raw.githubusercontent.com/rbicelli/pfsense-zabbix-template/master/pfsense_zbx.php
 
-# Remove parâmetros UserParameter existentes
-echo "Removendo parâmetros UserParameter existentes..."
-grep -v '^UserParameter' "$ZABBIX_CONF" > /tmp/zabbix_agentd.conf && mv /tmp/zabbix_agentd.conf "$ZABBIX_CONF"
+# Remove parâmetros de usuário existentes do Zabbix Agent
+echo "Removendo parâmetros de usuário existentes..."
+sed -i '' '/^UserParameter=/d' /usr/local/etc/zabbix6/zabbix_agentd.conf
 
-# Adiciona as configurações dos User Parameters no final do arquivo de configuração do Zabbix
+# Configura os novos parâmetros de usuário para o Zabbix Agent
 echo "Configurando Zabbix Agent..."
 {
-    echo ""
-    echo "# User Parameters"
     echo "AllowRoot=1"
     echo "UserParameter=pfsense.states.max,grep \"limit states\" /tmp/rules.limits | cut -f4 -d ' '"
     echo "UserParameter=pfsense.states.current,grep \"current entries\" /tmp/pfctl_si_out | tr -s ' ' | cut -f4 -d ' '"
@@ -24,20 +19,18 @@ echo "Configurando Zabbix Agent..."
     echo "UserParameter=pfsense.mbuf.max,netstat -m | grep \"mbuf clusters\" | cut -f1 -d ' ' | cut -d '/' -f4"
     echo "UserParameter=pfsense.discovery[*],/usr/local/bin/php /root/scripts/pfsense_zbx.php discovery \$1"
     echo "UserParameter=pfsense.value[*],/usr/local/bin/php /root/scripts/pfsense_zbx.php \$1 \$2 \$3"
-} >> "$ZABBIX_CONF"
+} >> /usr/local/etc/zabbix6/zabbix_agentd.conf
 
-# Habilita o Zabbix Agent no /etc/rc.conf
-echo "Habilitando Zabbix Agent no /etc/rc.conf..."
-if ! grep -q 'zabbix_agentd_enable="YES"' /etc/rc.conf; then
-    echo 'zabbix_agentd_enable="YES"' >> /etc/rc.conf
-fi
-
-# Aumenta o valor do timeout para 30 no arquivo de configuração do Zabbix
+# Aumenta o valor do timeout para 30
 echo "Aumentando o Timeout para 30..."
-sed -i '' 's/^# Timeout=3/Timeout=30/' "$ZABBIX_CONF"
+sed -i '' 's/^# Timeout=10/Timeout=30/' /usr/local/etc/zabbix6/zabbix_agentd.conf
 
-# Reinicia o serviço do Zabbix Agent com 'onerestart'
+# Reinicia o serviço do Zabbix Agent para aplicar as mudanças
 echo "Reiniciando o Zabbix Agent..."
-service zabbix_agentd onerestart
+service zabbix6_agentd restart
 
-echo "Script finalizado com sucesso!"
+# Verifica o status do serviço
+echo "Verificando o status do Zabbix Agent..."
+service zabbix6_agentd status
+
+echo "Configuração do Zabbix Agent concluída!"
