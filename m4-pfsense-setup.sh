7a7d0570d77312fc9d2ce20199b2919c51fd42f5
m4services <murilo.melo@m4services.com.br>
2024-09-30 15:08:17 -0300

    Update m4-pfsense-setup.sh

--- m4-pfsense-setup.sh
+++ m4-pfsense-setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Ativa o modo de depuração para exibir todos os comandos executados
+set -x
+
 # Baixa o arquivo pfsense_zbx.php para o diretório /root/scripts
 echo "Baixando o arquivo pfsense_zbx.php..."
 curl --create-dirs -o /root/scripts/pfsense_zbx.php https://raw.githubusercontent.com/rbicelli/pfsense-zabbix-template/master/pfsense_zbx.php
@@ -11,8 +14,8 @@ AllowRoot=1
 UserParameter=pfsense.states.max,grep "limit states" /tmp/rules.limits | cut -f4 -d ' '
 UserParameter=pfsense.states.current,grep "current entries" /tmp/pfctl_si_out | tr -s ' ' | cut -f4 -d ' '
 UserParameter=pfsense.mbuf.current,netstat -m | grep "mbuf clusters" | cut -f1 -d ' ' | cut -d '/' -f1
-UserParameter=pfsense.mbuf.cache,netstat -m | grep "mbuf clusters" | cut -f1 -d ' ' | cut -d '/' -f2
-UserParameter=pfsense.mbuf.max,netstat -m | grep "mbuf clusters" | cut -f1 -d ' ' | cut -d '/' -f4
+UserParameter=pfsense.mbuf.cache,netstat -m | cut -f1 -d ' ' | cut -d '/' -f2
+UserParameter=pfsense.mbuf.max,netstat -m | cut -f1 -d ' ' | cut -d '/' -f4
 UserParameter=pfsense.discovery[*],/usr/local/bin/php /root/scripts/pfsense_zbx.php discovery \$1
 UserParameter=pfsense.value[*],/usr/local/bin/php /root/scripts/pfsense_zbx.php \$1 \$2 \$3
 EOF
@@ -31,9 +34,11 @@ pkg update && pkg install -y py311-speedtest-cli-2.1.3
 /usr/local/bin/php /root/scripts/pfsense_zbx.php sysversion_cron
 
 # Corrige a instalação do speedtest caso necessário
+echo "Corrigindo a instalação do Speedtest (se necessário)..."
 curl -Lo /usr/local/lib/python3.11/site-packages/speedtest.py https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
 
 # Testa a instalação do Speedtest
+echo "Testando o Speedtest..."
 /usr/local/bin/speedtest
 
 # Configura o cronjob para rodar o speedtest regularmente
@@ -41,3 +46,6 @@ echo "Configurando o cronjob do Speedtest..."
 /usr/local/bin/php /root/scripts/pfsense_zbx.php speedtest_cron
 
 echo "Script finalizado com sucesso!"
+
+# Desativa o modo de depuração
+set +x
